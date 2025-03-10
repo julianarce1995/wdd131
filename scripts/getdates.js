c4cefb7c0dad983e89bbdb43c549b4b2e1ce7a59
Julian Arce <julianarce1101@gmail.com>
2025-03-10 17:38:45 -0500

    week 1 WDD 131: Dynamic Web Fundamentals

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+const currentyear = document.querySelector("#currentyear");
+const today = new Date();
+currentyear.textContent = `${today.getFullYear()}`;
+
+const lastModified = document.querySelector("#lastModified");
+const nLastModif = document.lastModified;
+lastModified.innerHTML = `last modified: ${nLastModif.toLocaleString()}`;
+
